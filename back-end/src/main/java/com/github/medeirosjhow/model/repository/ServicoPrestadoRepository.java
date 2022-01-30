d6b54660f5d38c48afb60e905dbda02c760a46d2
medeiros.jhow <jhonatas.medeiro#gmail.com>
2022-01-30 20:11:39 -0300

    Feat/Criado o endPoint de busca serviços prestados.

--- back-end/src/main/java/com/github/medeirosjhow/model/repository/ServicoPrestadoRepository.java
+++ back-end/src/main/java/com/github/medeirosjhow/model/repository/ServicoPrestadoRepository.java
@@ -1,8 +1,17 @@
 package com.github.medeirosjhow.model.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.github.medeirosjhow.model.entity.ServicoPrestado;
 
 public interface ServicoPrestadoRepository extends JpaRepository<ServicoPrestado, Integer> {
+
+	@Query(" select s from ServicoPrestado s join s.cliente c " + 
+	" where upper( c.nome ) like upper( :nome ) and  MONTH(s.data) =:mes ")
+	List<ServicoPrestado> findByNomeClienteAndMes(
+			@Param("nome") String nome, @Param("mes") Integer mes);
 }
