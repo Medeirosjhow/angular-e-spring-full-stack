62d09bebd3e29b51c41d53881ee43048c78f8c11
medeiros.jhow <jhonatas.medeiro#gmail.com>
2022-02-05 22:45:32 -0300

    Implementação do metodo de segurança e autorização

--- back-end/src/main/java/com/github/medeirosjhow/model/repository/UsuarioRepository.java
+++ back-end/src/main/java/com/github/medeirosjhow/model/repository/UsuarioRepository.java
@@ -1,9 +1,12 @@
 package com.github.medeirosjhow.model.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.github.medeirosjhow.model.entity.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
+	Optional<Usuario> findByUsername(String username);
 }
